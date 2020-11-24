c80687506d18f5f7653f5861d75f65780c8ddf34
tsingsan <itsingsan@gmail.com>
2020-11-24 17:18:06 +0800

    update

--- task_tsingsan.py
+++ task_tsingsan.py
@@ -5,9 +5,12 @@ import signal
 
 from Arknights.shell_next import _create_helper
 
-tasks = [("1-7", 99)]
+tasks_1_7 = [("1-7", 99)]
+tasks_ce_5 = [("CE-5", 99)]
 
 if __name__ == '__main__':
+
+    curHour = int(time.ctime()[-13:-11])
 
     helper = _create_helper()
     helper.use_refill = True
@@ -15,7 +18,7 @@ if __name__ == '__main__':
     with helper._shellng_with:
         helper.main_handler(
             clear_tasks=False,
-            task_list=tasks,
+            task_list=tasks_ce_5 if curHour < 20 else tasks_1_7,
             auto_close=False
         )
         helper.my_building()
