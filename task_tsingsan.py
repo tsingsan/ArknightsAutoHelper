03eea52a93cc016a152050e868d3f2a49f9b03ac
tsingsan <itsingsan@gmail.com>
2021-01-05 23:43:25 +0800

    do battle argv

--- task_tsingsan.py
+++ task_tsingsan.py
@@ -14,21 +14,24 @@ if __name__ == '__main__':
     curHour = int(time.ctime()[-13:-11])
     do_ce_5 = weekDay == "Tue" or weekDay == "Thu" or weekDay == "Sat" or weekDay == "Sun"
 
+    do_battle = sys.argv[1] != "nobattle" if len(sys.argv) > 1 else True
+
     helper = _create_helper()
     helper.use_refill = True
     helper.refill_with_item = True
     with helper._shellng_with:
-        if do_ce_5:
+        if do_battle:
+            if do_ce_5:
+                helper.main_handler(
+                    clear_tasks=False,
+                    task_list=tasks_ce_5,
+                    auto_close=False
+                )
             helper.main_handler(
                 clear_tasks=False,
-                task_list=tasks_ce_5,
+                task_list=tasks_1_7,
                 auto_close=False
             )
-        helper.main_handler(
-            clear_tasks=False,
-            task_list=tasks_1_7,
-            auto_close=False
-        )
         helper.my_building()
         helper.recruit_daily()
         helper.get_credit()
